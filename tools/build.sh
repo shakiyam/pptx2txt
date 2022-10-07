d6715fa1e003429fc261e5698628dbcfb15ba0ed
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-10-07 10:58:33 +0900

    Fix issue with image name starting with docker.io

--- tools/build.sh
+++ tools/build.sh
@@ -20,10 +20,10 @@ readonly DOCKERFILE
 
 DOCKER=$(command -v docker || command -v podman)
 readonly DOCKER
-CURRENT_IMAGE="$($DOCKER image ls -q "$IMAGE_NAME":latest)"
+CURRENT_IMAGE="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME":latest || :)"
 readonly CURRENT_IMAGE
 $DOCKER image build -f "$DOCKERFILE" -t "$IMAGE_NAME" .
-LATEST_IMAGE="$($DOCKER image ls -q "$IMAGE_NAME":latest)"
+LATEST_IMAGE="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME":latest || :)"
 readonly LATEST_IMAGE
 if [[ "$CURRENT_IMAGE" != "$LATEST_IMAGE" ]]; then
   $DOCKER image tag "$IMAGE_NAME":latest "$IMAGE_NAME":"$(date +%Y%m%d%H%S)"
