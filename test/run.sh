f8960a32bb252c51ff593dcf27439502781d2aa6
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-07-17 07:22:41 +0000

    Fix test script to properly validate sample3.txt output

--- test/run.sh
+++ test/run.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 ../pptx2txt sample1.pptx sample2.pptx sample3.pptx
 diff sample1.txt expected/sample1.txt
 diff sample2.txt expected/sample2.txt
-diff sample2.txt expected/sample2.txt
+diff sample3.txt expected/sample3.txt
