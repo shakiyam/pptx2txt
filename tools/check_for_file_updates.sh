a078097555ec03824766f00fce96507ec6514741
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-09-16 08:01:51 +0000

    Add colored status messages to file update checker

--- tools/check_for_file_updates.sh
+++ tools/check_for_file_updates.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -Eeu -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly SCRIPT_DIR
+# shellcheck disable=SC1091
+. "$SCRIPT_DIR"/colored_echo.sh
+
 if [[ -n $(git diff "$@") ]]; then
+  echo_error "File updates detected:"
   git --no-pager diff "$@"
   exit 2
 fi
+echo_success "No file changes detected."
