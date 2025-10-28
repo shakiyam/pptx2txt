2ba3eef4ab6309babb62831cdf94d098e764392b
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-10-28 08:24:14 +0000

    Add type hints to create_sendto_shortcut.py for mypy compliance

--- create_sendto_shortcut.py
+++ create_sendto_shortcut.py
@@ -1,11 +1,16 @@
 import os.path
 import sys
+from typing import Any
+
+if sys.platform != "win32":
+    print("Error: This script only works on Windows", file=sys.stderr)
+    sys.exit(1)
 
 from win32com.client import Dispatch
 
-shell = Dispatch("WScript.Shell")
-sendto_dir = shell.SpecialFolders("SendTo")
-shortcut = shell.CreateShortCut(os.path.join(sendto_dir, "pptx2txt.lnk"))
-shortcut.Targetpath = sys.executable
+shell: Any = Dispatch("WScript.Shell")
+sendto_dir: str = shell.SpecialFolders("SendTo")
+shortcut: Any = shell.CreateShortCut(os.path.join(sendto_dir, "pptx2txt.lnk"))
+shortcut.TargetPath = sys.executable
 shortcut.Arguments = os.path.join(os.path.dirname(__file__), "pptx2txt.py")
-shortcut.save()
+shortcut.Save()
